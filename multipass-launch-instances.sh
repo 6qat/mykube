3b63fade75099031758e471cbb271e509ad81a6f
Guilherme Moreira <ceschiatti@gmail.com>
2020-06-12 21:07:18 -0300

    How to correct multipass bug

--- multipass-launch-instances.sh
+++ multipass-launch-instances.sh
@@ -1,2 +1,6 @@
+sudo snap install multipass --classic
+sudo multipass set local.driver=libvirt
+sudo snap connect multipass:libvirt
+
 multipass launch --name server -c 2 --cloud-init ./cloud-config.yaml 20.04
 
