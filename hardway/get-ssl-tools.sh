c098523dc13877a0282928b16b01f3288558b191
Guilherme Moreira <ceschiatti@gmail.com>
2020-06-18 13:40:50 -0300

    Getting ssl tools

---
+++ hardway/get-ssl-tools.sh
@@ -0,0 +1,4 @@
+wget -q --show-progress --https-only --timestamping \
+  https://storage.googleapis.com/kubernetes-the-hard-way/cfssl/linux/cfssl \
+  https://storage.googleapis.com/kubernetes-the-hard-way/cfssl/linux/cfssljson
+
