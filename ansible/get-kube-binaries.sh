1fc4fe3f017154cc640ea63adfad0e4098fc170a
Guilherme Moreira <ceschiatti@gmail.com>
2020-06-21 10:44:32 -0300

    Script to download kube binaries locally

---
+++ ansible/get-kube-binaries.sh
@@ -0,0 +1,10 @@
+VERSION="v1.18.4"
+
+wget -q --show-progress --https-only --timestamping -P bin/ \
+	  	https://storage.googleapis.com/kubernetes-release/release/${VERSION}/bin/linux/amd64/kube-apiserver \
+	    	https://storage.googleapis.com/kubernetes-release/release/${VERSION}/bin/linux/amd64/kube-controller-manager \
+	      	https://storage.googleapis.com/kubernetes-release/release/${VERSION}/bin/linux/amd64/kube-scheduler \
+	        https://storage.googleapis.com/kubernetes-release/release/${VERSION}/bin/linux/amd64/kubectl \
+		https://storage.googleapis.com/kubernetes-release/release/${VERSION}/bin/linux/amd64/kube-proxy \
+		https://storage.googleapis.com/kubernetes-release/release/${VERSION}/bin/linux/amd64/kubelet \
+		https://storage.googleapis.com/kubernetes-release/release/${VERSION}/bin/linux/amd64/kubeadm 
