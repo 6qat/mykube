d265c9e580f4b14a65b225edd6c4e73638b1899b
Guilherme Moreira <ceschiatti@gmail.com>
2020-06-23 18:29:46 +0000

    Improving

--- multipass-launch-instances.sh
+++ multipass-launch-instances.sh
@@ -7,3 +7,4 @@ sudo snap connect multipass:libvirt
 
 multipass launch --name server -c 2 --cloud-init ./cloud-config.yaml 20.04
 
+multipass launch -c 2 -m 2G --cloud-init ./cloud-config.yaml -n master-0 18.04
