a31572e850720ad0957e29fdc17ac0b2fe14ff6a
Guilherme Ceschiatti B. Moreira <ceschiatti@gmail.com>
2023-06-29 15:07:10 -0300

    Add more vm preparatives

--- multipass-launch-instances.sh
+++ multipass-launch-instances.sh
@@ -10,3 +10,6 @@ multipass launch --name server -c 2 --cloud-init ./cloud-config.yaml 20.04
 multipass launch -c 2 -m 2G --cloud-init ./cloud-config.yaml -n master-0 18.04
 
 multipass launch -c 2 -m 4G -d 6G --cloud-init ./cloud-config.yaml -n worker-0 18.04
+
+multipass launch -c 4 -m 4G -d 800G -n btc 22.04 --network "Wi-Fi"
+
